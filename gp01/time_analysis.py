f865921da753665a9d714330d47e43c544cbdd99
Mingxi <zhu@Mingxis-MacBook-Pro.local>
2026-05-29 13:09:21 -0700

    add

--- gp01/time_analysis.py
+++ gp01/time_analysis.py
@@ -3,6 +3,14 @@ import csv
 
 with open("./gp01/time.csv", mode="r", newline='', encoding="utf-8") as f:
     csv_reader = csv.reader(f)
-    time=(double)csv_reader[0].split(",")
-    cases=[10, 100, 1000]
-    
+    time = []
+    for row in csv_reader:
+        for t in row:
+            time.append(float(t))
+    cases=[i for i in range(10, 1001, 10)]
+    plt.plot(time, cases)
+    plt.xlabel("time: ms")
+    plt.ylabel("string length")
+    plt.title("Time analysis for Min Cost To Convert String II")
+    plt.show()
+
